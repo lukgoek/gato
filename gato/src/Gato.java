56deb9626a7e92c5036096a6fcaaebbe2b753c30
Humberto Lugo Aguilar <lugoe_@hotmail.com>
2015-02-05 16:48:07 -0700

    *Se crearon las pantallas pricipales

--- gato/src/Gato.java
+++ gato/src/Gato.java
@@ -1,3 +1,6 @@
+
+import gato.mainMenu;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -14,7 +17,9 @@ public class Gato {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        mainMenu obj = new mainMenu();
+        obj.setVisible(true);
     }
     
 }
